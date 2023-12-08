1cec1bbe2bfd1a8d563ff265eae6fb1a86a2e597
César Rodriguez <54708668+ironsand-sou-eu@users.noreply.github.com>
2023-12-08 17:40:19 +0100

    Started developing filter

---
+++ src/components/macro/TopFilter.tsx
@@ -0,0 +1,11 @@
+type TopFilterProps = {};
+
+export default function TopFilter({}: TopFilterProps): JSX.Element {
+  // SELECT SINGLE Tipo de registro (tarefa, andamento, timesheet)
+  // SELECT MULTI Tipo de tarefa
+  // Data inicial e final
+  // SELECT MULTI Responsável
+  // SELECT MULTI Núcleo
+  // SELECT MULTI Status
+  return <aside className="filter"></aside>;
+}
