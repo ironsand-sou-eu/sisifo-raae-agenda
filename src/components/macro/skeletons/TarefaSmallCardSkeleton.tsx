ded9cf073f538a427659fba7fd993945ba9e14d7
César Rodriguez <54708668+ironsand-sou-eu@users.noreply.github.com>
2023-12-22 00:05:43 +0100

    Created small card loading skeleton

---
+++ src/components/macro/skeletons/TarefaSmallCardSkeleton.tsx
@@ -0,0 +1,20 @@
+export default function TarefaSmallCardSkeleton(): JSX.Element {
+  return (
+    <section className="tarefa-card">
+      <header className="tarefa-card-titulo">
+        <div>
+          <div className="skeleton skeleton-circle" />
+          <div className="skeleton skeleton-header" />
+        </div>
+        <div className="skeleton skeleton-buttons-area" />
+      </header>
+      <div className="processo-info">
+        <p className="skeleton skeleton-text" />
+        <p className="skeleton skeleton-text" />
+        <p className="skeleton skeleton-text" />
+        <p className="skeleton skeleton-text" />
+      </div>
+      <footer className="skeleton skeleton-text"></footer>
+    </section>
+  );
+}
