c71d727f390e006ecb1a1b89a942cb9e15c37403
César Rodriguez <54708668+ironsand-sou-eu@users.noreply.github.com>
2024-01-11 22:32:54 +0100

    Adjusted textarea

--- src/components/micro/Textarea.tsx
+++ src/components/micro/Textarea.tsx
@@ -1,16 +1,21 @@
+import { ChangeEventHandler } from "react";
+
 type TextareaProps = {
   nameAndId: string;
   label: string;
+  onChange: (newValue: string) => void;
   content?: string;
 };
 
-export default function Textarea({ nameAndId, label, content }: TextareaProps): JSX.Element {
+export default function Textarea({ nameAndId, label, content, onChange }: TextareaProps): JSX.Element {
   return (
     <div>
       <label className="sisifo-label" htmlFor={nameAndId}>
         {label}
       </label>
-      <textarea id={nameAndId} name={nameAndId} defaultValue={content} />
+      <textarea id={nameAndId} name={nameAndId} onChange={({ target }) => onChange(target.value)}>
+        {content}
+      </textarea>
     </div>
   );
 }
